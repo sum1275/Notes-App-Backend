51ac1ae4203a3e1b7da142dd0f633126f0fda27f
sum1275 <sumitsinha215@gmail.com>
2025-01-14 22:08:42 +0530

    same site issue

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -52,7 +52,7 @@ export const signin = async (req, res, next) => {
 
     const { password: pass, ...rest } = validUser._doc
 
-    res.cookie("access_token", token, { httpOnly: true }).status(200).json({
+    res.cookie("access_token", token, { httpOnly: true, sameSite: 'None'}).status(200).json({
       success: true,
       message: "Login Successful!",
       rest,
